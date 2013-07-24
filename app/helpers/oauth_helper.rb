6540bbf2e783eda690e49358cbaf5d19ff88e5ae
Youssif <abdulyoussif@gmail.com>
2013-07-24 14:33:25 -0700

    removed oauth helper

--- app/helpers/oauth_helper.rb
+++
@@ -1,6 +0,0 @@
-module OauthHelper
-  @oauth = Koala::Facebook::OAuth.new(api_key, app_secret, callback_url)
-  @oauth.url_for_oauth_code
-  @oauth.url_for_oauth_code(:permissions => "publish_stream")
-  @oauth.url_for_access_token(code)
-end
