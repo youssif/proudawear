c58ded36acb7d329b62a87bbfb945039a166e393
MakerSquare Student <students@themakersquare.com>
2013-08-13 16:39:11 -0500

    added mouseenter and mouseleave funcitonality

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,16 +2,33 @@
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 
-$(document).ready(function(){
-  $('img').mouseenter(function(){
-    var image= $(this).find('img'), caption = $(this).find('div');
-    caption.width(image.width());
-    caption.height(image.height());
-    caption.fadeIn();
-  }).mouseleave(function(){
-    var image= $(this).find('img'), caption = $(this).find('div');
-    caption.width(image.width());
-    caption.height(image.height());
-    caption.fadeOut();
-  });
-});
+// Example mouse enter/leave funcitonality:
+// $(document).ready(function(){
+//   $('img').mouseenter(function(){
+//     var image= $(this).find('img'), caption = $(this).find('div');
+//     caption.width(image.width());
+//     caption.height(image.height());
+//     caption.fadeIn();
+//   }).mouseleave(function(){
+//     var image= $(this).find('img'), caption = $(this).find('div');
+//     caption.width(image.width());
+//     caption.height(image.height());
+//     caption.fadeOut();
+//   });
+// });
+
+
+// ----- Clears the 'Edit' and "Delete id selectors" -----
+  $('.edit').empty();
+  $('.delete').empty();
+
+  // $(document).ready(function(){
+    $('img').mouseenter(function(){
+			$('.edit').append("<a href='/view/clothes/1'>link me</a>");
+			$('.delete').append("Delete");
+    });
+    // $('img').mouseleave(function(){
+    // 	  $('.edit').empty();
+  		// 	$('.delete').empty();
+    // });
+  // )};
