76ed22cf951fffc39e53ac8a76719ca0717f494e
Youssif <abdulyoussif@gmail.com>
2013-08-14 14:53:51 -0700

    nav bar now fully responsive

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,14 +29,20 @@ $(function() {
   $(document).foundation();	
 });
 
+var originalContent = $('a#user_name').html();
+
 $(window).resize(function(){
-	if($(window).width()<550){
+	if($(window).width()<670){
 		$('img.facebook_login').attr('src', 'assets/smallfb.png');
 		$('a#fb_login').css('width','5em').css('margin-top','-0.4em').css('margin-right','0em');
+		$('a#user_name').text('')
+		$('ul.logged_in').css('margin-right','-1.3em')
 	}
 	else{
 		$('img.facebook_login').attr('src', 'assets/Facebook_login.png');
 		$('a#fb_login').css('width','20em').css('margin-top','-0.1em').css('margin-right','-4em');
-	}
+		$('a#user_name').html(originalContent);
+		$('ul.logged_in').css('margin-right','0em');
+	};
 })
   
